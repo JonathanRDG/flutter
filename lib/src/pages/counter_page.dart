90683848b25b459fa2862781f76c1c9a321b8cb5
jonathan <jr111_@hotmail.com>
2019-08-27 15:30:50 -0500

    Add the new bottons with row

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Counterpage extends StatefulWidget {
 
@@ -7,9 +8,9 @@ class Counterpage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<Counterpage> {
-
   final  _stytext = new TextStyle(fontSize: 40);
   int _counter = 0;
+  bool _validation= false;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -23,19 +24,44 @@ class _CounterPageState extends State<Counterpage> {
           children: <Widget>[
             Text('Numero de Taps: ', style: _stytext ),
             Text('$_counter', style: _stytext),
+
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _counter++;
-          setState(() {
-
-          });
-        },
-      ),
+      floatingActionButton: _createBotton()
     );
+  }
+
+  Widget _createBotton(){
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(width: 30),
+    FloatingActionButton(child: Icon(Icons.add),onPressed:
+        () {
+      _counter++;
+      setState(() {});
+    },),
+    SizedBox(width: 5),
+    FloatingActionButton(child: Icon(Icons.remove),onPressed:
+        () {
+          (_counter<=0)? _counter=0:_counter--;
+      setState(() {});
+    },
+    ),
+    SizedBox(width: MediaQuery.of(context).size.width*.42),
+        FloatingActionButton(child: Icon(Icons.exposure_zero),onPressed:
+            () {
+          _counter=0;
+          setState(() {});
+        },
+        ),
+        SizedBox(width: 5),
+      ],
+    );
+
+
+
   }
 }
